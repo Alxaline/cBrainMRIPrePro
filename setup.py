68bc13bbda67169ee251367fa9b56634b32c9459
Alxaline <xela.errac@gmail.com>
2022-08-01 10:36:00 +0200

    Correct typo comma in setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(install_requires=['torch',
                         'numba',
                         'argparse',
                         'numpy',
-                        'antspyx'
+                        'antspyx',
                         # 'antspyx @ git+https://github.com/ANTsX/ANTsPy.git@v0.2.6#egg=antspyx',
                         'HD-BET @ git+https://github.com/MIC-DKFZ/HD-BET.git#egg=HD-BET',
                         ], **args)
